61e1bb77289d47ac8bcce6b798fc20479a7cb852
abcdan <daniel@lngzl.nl>
2022-08-03 00:00:28 +0200

    this kinda works

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 /**
  * Note! I usually first design the programm in a way that it would be able to
  * the basic functions.
- * 
+ *
  * I'll then refactor the programm to make it more readable and usable.
- * 
- * So if you look at this file and think "Why isn't something split up", 
+ *
+ * So if you look at this file and think "Why isn't something split up",
  * look above for the answer.
  */
 require("dotenv").config();
@@ -37,22 +37,22 @@ app.get("/mail/:address", async (req, res) => {
   if (!connection) {
     return res.status(500).json({ msg: "mail server not running yet." });
   }
-  
+
   const start = new Date();
   const address = req.params.address;
-  const searchCriteria = [
-    ["TO", address],
-    ["SUBJECT", "*"],
-  ];
-  const results = await connection.search(searchCriteria);
+  var searchCriteria = [["HEADER", "envelope-to", address]];
+  // TODO: Fetch subject, body and from
+  var fetchOptions = { bodies: ["TEXT", ""], struct: false };
+  const results = await connection.search(searchCriteria, fetchOptions);
+  console.log(results);
   const end = new Date();
   const time = end.getTime() - start.getTime();
   res.json({ mails: results, time: `${time}ms`, query: address });
 });
 
 app.all("/ping", (req, res) => {
-    res.json({ msg: "pong", connected: !!connection});
-})
+  res.json({ msg: "pong", connected: !!connection });
+});
 
 app.listen(process.env.PORT ?? 80, () => {
   console.log(`📨 tmpx is live on port ${process.env.PORT ?? 80}`);
